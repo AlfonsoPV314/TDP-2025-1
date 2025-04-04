1390e9286e322c0f860cdcbae0b3d0aa18062ccd
AlfonsoPV314 <144069261+AlfonsoPV314@users.noreply.github.com>
2025-04-04 11:21:50 -0300

    Added boat and state .h files

---
+++ Boat.h
@@ -0,0 +1,7 @@
+#include <iostream>
+
+class Boat{
+    int PsgSize;
+    int FuelAmt;
+    int* Psgs;
+};
---
+++ State.h
@@ -0,0 +1,9 @@
+#include "Graph.h"
+#include "Boat.h"
+
+class State{
+    int* Isle1;
+    int* Isle2;
+    Graph M;
+    Boat* BoatArray;
+};
